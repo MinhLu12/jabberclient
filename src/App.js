b31caaa5d8697e7d5de9a65f2886f965a59f74a4
Minh Lu <Minh.Lu45@T-mobile.com>
2019-10-18 19:07:27 -0700

    Refactor App.js to be more readable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-
 import axios from 'axios';
 
 export default class App extends React.Component {
-  state = {
-    persons: Object
-  }
+  state = {}
 
   componentDidMount() {
     axios.get(`http://localhost:5000/test`)
       .then(res => {
-        const persons = res.data;
-        console.log(persons)
-        this.setState({ persons });
+        const testText = res.data;
+        this.setState({ testText });
       })
   }
 
   render() {
     return (
-      <ul>
-        { this.state.persons.title }
-      </ul>
+      <div>
+        { this.state.testText }
+      </div>
     )
   }
 }
